c4c91ba6bb71a64a6e8d87d8640c5c9212e54702
Shawn W <78707396+sb-son@users.noreply.github.com>
2023-01-02 12:27:14 -0600

    completed syntax and data types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello,");
-        System.out.print(" World!");
+        System.out.println(" World!");
+        int myFavoriteNumber;
+        myFavoriteNumber = 26;
+        double myString;
+        myString = 3.14;
+        float myNumber;
+        myNumber = 3;
+        System.out.println(myNumber);
+        System.out.print(myString);
+        System.out.println(myFavoriteNumber + ".");
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
     }
 }
